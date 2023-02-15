1c5547e7d0295089871581a877118bd065de01f4
Outvi V <git@outv.im>
2023-02-15 22:03:56 +0800

    fix(utils): don't alter chatId

--- m4-daemon/src/utils.ts
+++ m4-daemon/src/utils.ts
@@ -4,12 +4,5 @@ export function parseChatId(id: string | number): number | undefined {
     id = Number(id)
     if (Number.isNaN(id)) return undefined
   }
-  if (id < 0) {
-    if (String(id).startsWith('-100')) {
-      id = Number(String(id).replace(/^-100/, ''))
-    } else {
-      return undefined
-    }
-  }
   return id
 }
